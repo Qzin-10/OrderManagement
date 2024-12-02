3fc9b4c98a1078cfb7968e48bee5055f8a50d7e4
SrishanthReddy <jakkasrishanthreddy@gmail.com>
2024-12-02 23:38:55 +0530

    First

---
+++ src/main/java/com/Qzin/QzinApplication.java
@@ -0,0 +1,13 @@
+package com.Qzin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QzinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Qzin/QzinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Qzin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QzinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
